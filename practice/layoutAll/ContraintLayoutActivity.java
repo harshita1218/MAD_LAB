fd145c325f7cc3fd64dfcb2aa7137811dadfb1bb
harshita1218 <117938497+harshita1218@users.noreply.github.com>
2025-02-05 09:33:51 +0530

    Create ContraintLayoutActivity.java

---
+++ practice/layoutAll/ContraintLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.example.layoutdemo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
