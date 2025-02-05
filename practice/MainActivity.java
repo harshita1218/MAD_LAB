d7ad96bd38052a3650ad83b527b26f7379d39e45
harshita1218 <117938497+harshita1218@users.noreply.github.com>
2025-02-05 09:25:25 +0530

    Create MainActivity.java

---
+++ practice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toastapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button toastButton = findViewById(R.id.toastButton);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello, this is a Toast message!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
