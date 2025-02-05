8f9ff97920774f42c5c6d87c0efa13a8325fbcb7
harshita1218 <117938497+harshita1218@users.noreply.github.com>
2025-02-05 09:32:50 +0530

    Create FrameLayout.java

---
+++ practice/layoutAll/FrameLayout.java
@@ -0,0 +1,12 @@
+package com.example.layoutdemo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FrameLayoutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_layout);
+    }
+}
