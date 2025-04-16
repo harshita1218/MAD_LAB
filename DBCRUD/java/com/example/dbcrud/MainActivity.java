ccab9944dfb870cae114d1c96df7e13ec05678a1
harshita1218 <117938497+harshita1218@users.noreply.github.com>
2025-04-16 13:39:14 +0530

    Add files via upload

---
+++ DBCRUD/java/com/example/dbcrud/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.dbcrud;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // creating variables for our edittext, button and dbhandler
+    private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
+    private Button addCourseBtn, readCourseBtn;
+    private DBHandler dbHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // initializing all our variables.
+        courseNameEdt = findViewById(R.id.idEdtCourseName);
+        courseTracksEdt = findViewById(R.id.idEdtCourseTracks);
+        courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
+        courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription);
+        addCourseBtn = findViewById(R.id.idBtnAddCourse);
+        readCourseBtn = findViewById(R.id.idBtnReadCourse);
+
+        // creating a new dbhandler class
+        // and passing our context to it.
+        dbHandler = new DBHandler(MainActivity.this);
+
+        // below line is to add on click listener for our add course button.
+        addCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // below line is to get data from all edit text fields.
+                String courseName = courseNameEdt.getText().toString();
+                String courseTracks = courseTracksEdt.getText().toString();
+                String courseDuration = courseDurationEdt.getText().toString();
+                String courseDescription = courseDescriptionEdt.getText().toString();
+
+                // validating if the text fields are empty or not.
+                if (courseName.isEmpty() || courseTracks.isEmpty() || courseDuration.isEmpty() || courseDescription.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter all the data..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // on below line we are calling a method to add new
+                // course to sqlite data and pass all our values to it.
+                dbHandler.addNewCourse(courseName, courseDuration, courseDescription, courseTracks);
+
+                // after adding the data we are displaying a toast message.
+                Toast.makeText(MainActivity.this, "Course has been added.", Toast.LENGTH_SHORT).show();
+                courseNameEdt.setText("");
+                courseDurationEdt.setText("");
+                courseTracksEdt.setText("");
+                courseDescriptionEdt.setText("");
+            }
+        });
+
+        readCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opening a new activity via a intent.
+                Intent i = new Intent(MainActivity.this, ViewCourses.class);
+                startActivity(i);
+            }
+        });
+    }
+
+      @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_example, menu);
+            return true;
+        }
+
+        // Overriding onOptionsItemSelected Method
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+
+            if (id == R.id.send) {
+                Toast.makeText(getApplicationContext(),
+                        "Shows share icon", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.gallery) {
+                Toast.makeText(getApplicationContext(),
+                        "Shows image icon", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.call) {
+                Toast.makeText(getApplicationContext(),
+                        "Shows call icon", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.calculator) {
+                Toast.makeText(getApplicationContext(),
+                        "Calculator menu", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.exit) {
+                finish();
+                return true;
+            } else {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+
+}
+
+
+
+
